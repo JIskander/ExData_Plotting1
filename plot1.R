1da2bc0ae004de05adeb538aff7c6311db84d2e3
JIskander <julie.iskander@gmail.com>
2015-06-07 07:27:52 +0300

    plot1 & 2 updated

--- plot1.R
+++ plot1.R
@@ -2,12 +2,13 @@
 url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 temp <- tempfile()  ## creating temporary file
 zdata<-download.file(url,temp,method="curl")  
-fileUrl<-unz(temp, "household_power_consumption.txt")  ## unzipping downloaded file
+fname<-"household_power_consumption.txt"
+unzip(temp, fname)  ## unzipping downloaded file
 
 nrows<-24*60*2  ## number of rows to read
-header <- read.table(fileUrl, nrows = 1, header = FALSE, sep =';', stringsAsFactors = FALSE)
-DT<-read.table(fileUrl,sep=";",header = FALSE,na.strings = "?", skip=grep("1/2/2007", readLines(fileUrl)),nrows = nrows)
-colnames( DT ) <- unlist(header)
+header <- fread(fname, nrows = 1, header = FALSE, sep =';', stringsAsFactors = FALSE)
+DT<-fread(fname,sep=";",header = FALSE,na.strings = "?", skip="1/2/2007",nrows = nrows)
+setnames(DT,colnames(DT), as.character(header))
 
 png('plot1.png')
 hist(DT$Global_active_power,col="red",main="Global Active Power",  xlab="Global Active Power (kilowatts)", ylim=c(0,1200))
--- plot2.R
+++ plot2.R
@@ -1,10 +1,14 @@
-setwd("/Volumes/Julie/Coursera/Exploratory Data Analysis/repo")
-fileUrl<-"../household_power_consumption.txt"
+url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp <- tempfile()  ## creating temporary file
+zdata<-download.file(url,temp,method="curl")  
+fname<-"household_power_consumption.txt"
+unzip(temp, fname)  ## unzipping downloaded file
+
 nrows<-24*60*2
-header <- fread(fileUrl, nrows = 1, header = FALSE, sep =';', stringsAsFactors = FALSE)
-DT<-fread(fileUrl,sep=";",header = FALSE,na.strings = "?",stringsAsFactors = FALSE, skip="1/2/2007",nrows = nrows)
+header <- fread(fname, nrows = 1, header = FALSE, sep =';', stringsAsFactors = FALSE)
+DT<-fread(fname,sep=";",header = FALSE,na.strings = "?",stringsAsFactors = FALSE, skip="1/2/2007",nrows = nrows)
 setnames(DT,colnames(DT), as.character(header))
 DT=DT[,FullDate:=as.character(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))]
-png('plot1.png')
+png('plot2.png')
 plot(as.POSIXct(DT$FullDate),DT$Global_active_power, type="l",xlab="",ylab = "Global Active Power (kilowatts)")
 dev.off()
